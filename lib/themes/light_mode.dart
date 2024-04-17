563e742423932c9fa1900618f623d4429c013dcc
GeoffNderitu <geoffreynderitun@gmail.com>
2024-04-17 18:03:55 +0300

    Light_mode theme

---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    background: Colors.grey.shade400,
+    primary: Colors.red.shade500,
+    secondary: Colors.red.shade100,
+    tertiary: Colors.white,
+    inversePrimary: Colors.red.shade700
+  )
+);
