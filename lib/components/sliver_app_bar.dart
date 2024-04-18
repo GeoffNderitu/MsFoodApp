5e751ac147c8604f10b762424441648348d0dc78
GeoffNderitu <geoffreynderitun@gmail.com>
2024-04-18 13:20:14 +0300

    Sliver app bar here

---
+++ lib/components/sliver_app_bar.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class MsFSliverAppBar extends StatelessWidget {
+  const MsFSliverAppBar({
+    super.key, 
+    required this.child, 
+    required this.title,
+  });
+
+  final Widget child;
+  final  Widget title;
+  @override
+  Widget build(BuildContext context) {
+    return SliverAppBar(
+      expandedHeight: 340,
+      collapsedHeight: 120,
+      floating: false,
+      pinned: false,
+      actions: [
+        // Cart btn
+        IconButton(onPressed: () {}, 
+        icon: const Icon(Icons.shopping_cart))
+      ],
+      backgroundColor: Theme.of(context).colorScheme.background,
+      foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+      title: const Text("MySoko Food"),
+      flexibleSpace: FlexibleSpaceBar(
+        background: Padding(
+          padding: const EdgeInsets.only(bottom: 50.0),
+          child: child,
+        ),
+        title: title,
+        centerTitle: true,
+        titlePadding: const EdgeInsets.only(left: 0, right: 0, top: 0),
+        expandedTitleScale: 1,
+      ),
+    );
+  }
+}
