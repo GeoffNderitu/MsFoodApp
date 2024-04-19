10a8670dc8d171595926069c0ae26d37dc2d475a
GeoffNderitu <geoffreynderitun@gmail.com>
2024-04-19 12:10:53 +0300

    New page for food

---
+++ lib/pages/food_page.dart
@@ -0,0 +1,139 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:m_s_food/components/buttons.dart';
+
+import '../models/food.dart';
+
+class FoodPage extends StatefulWidget {
+  FoodPage({
+    super.key, 
+    required this.food,
+  }){
+    // Initialize selcted addons to be false
+     for (Addon addon in food.availableAddons){
+      selectedAddons[addon] = false;
+  }
+  }
+  final Food food;
+  final Map<Addon, bool> selectedAddons = {};
+  @override
+  State<FoodPage> createState() => _FoodPageState();
+}
+
+class _FoodPageState extends State<FoodPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Scaffold(
+          body: SingleChildScrollView(
+            child: Column(
+              children: [
+              // Food Img
+              Image.asset(widget.food.imagePath),
+              // Food name
+              Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      widget.food.name, 
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                       ),
+                    ),
+            
+                    // Food Price
+                    Text(
+                     "Ksh${widget.food.price}",
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Theme.of(context).colorScheme.primary,
+                       ),
+                      ),
+            
+                      const SizedBox(height: 10),
+                    // Food desc
+                    Text(
+                      widget.food.description,
+                      ),
+            
+                      const SizedBox(height: 10),
+                      
+                      Divider(color: Theme.of(context).colorScheme.secondary),
+            
+                      const SizedBox(height: 10),
+                    // Add ons
+                    Text(
+                        "Add-ons", 
+                        style: TextStyle(
+                          color: Theme.of(context).colorScheme.inversePrimary, 
+                          fontSize: 16, 
+                          fontWeight: FontWeight.bold,
+                      ),
+                      ),
+                      const SizedBox(height: 10),
+            
+                    Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: ListView.builder(
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        padding: EdgeInsets.zero,
+                        itemCount: widget.food.availableAddons.length,
+                        itemBuilder: (context, index){
+                          // Get individual addon
+                          Addon addon = widget.food.availableAddons[index];
+                        return CheckboxListTile(
+                          title: Text(addon.name),
+                          subtitle: Text("Ksh${addon.price}", 
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                          ),
+                          value: widget.selectedAddons[addon],
+                          onChanged: (bool? value) {
+                            setState(() {
+                              widget.selectedAddons[addon] = value!;
+                            });
+                          },
+                        );
+                      }),
+                    ),
+                  ],
+                ),
+              ),
+              // Btn(Add to Cart) 
+              MsFButton(text: "Add To Cart", onTap: () {}),
+        
+              const SizedBox(height: 25),
+            ],
+            ),
+          ),
+        ),
+        SafeArea(
+          child: Opacity(
+            opacity: 0.6,
+            child: Container(
+              margin: const EdgeInsets.only(left: 25),
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                shape: BoxShape.circle,
+              ),
+              child: IconButton(icon: const Icon(Icons.arrow_back_ios_rounded),
+              onPressed: () => Navigator.pop(context),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
